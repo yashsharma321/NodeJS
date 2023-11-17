38f816aead22e68e89cc1f899a376542e088c0af
yashsharma <yash.sharma@cloud.com>
2023-11-17 11:42:28 +0530

    bug fix

--- mern/restapi/empapi.js
+++ mern/restapi/empapi.js
@@ -28,28 +28,35 @@ router.post("/", async (req, res) => {
   } catch (error) {
     // Handle any errors that occur during the creation process
     res.status(500).json({ error: "Failed to add new employee" });
-  }s
+  }
 });
 
 // to update the record
 router.patch("/", async (req, res) => {
-  let empdetails = await Employee.findById(req.body.id);
-  if (req.body.empname != "") {
-    // to check if empname is comming from frontend
-    empdetails.empname = req.body.empname;
-  }
-  if (req.body.dept != "") {
-    empdetails.dept = req.body.dept;
-  }
-  if (req.body.city != "") {
-    empdetails.city = req.body.city;
-  }
-  if (req.body.salary != "") {
-    empdetails.salary = req.body.salary;
-  }
+  try {
+    const empdetails = await Employee.findById(req.body.id);
+    if (!empdetails) {
+      return res.status(404).json({ error: "Employee not found" });
+    }
 
-  empdetails.save();
-  res.status(201).json(empdetails);
+    if (req.body.empname) {
+      empdetails.empname = req.body.empname;
+    }
+    if (req.body.dept) {
+      empdetails.dept = req.body.dept;
+    }
+    if (req.body.city) {
+      empdetails.city = req.body.city;
+    }
+    if (req.body.salary) {
+      empdetails.salary = req.body.salary;
+    }
+
+    await empdetails.save();
+    res.status(200).json(empdetails);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to update record" });
+  }
 });
 
 // to delete the  record
